f0a4ed8093f2374edfeb94f2217a689528323546
JorgeNunes <jlbnunes@live.com>
2024-11-11 09:06:51 -0300

    refactor:new exchange

--- src/boot/helpFilter.js
+++ src/boot/helpFilter.js
@@ -36,11 +36,11 @@ const filters = {
     }).format(value);
   },
   /**
-   * converte para dolar data 05/11 $exchange
+   * converte para dolar data 11/11 $exchange
    * @param {String|Number} value valor que sera convertido para o cambio
    * @param {Number|null} exchange valor do câmbio para ser dividido no calculo
    */
-  convertCoin(value, exchange = 5.7005011948) {
+  convertCoin(value, exchange = 5.700501195) {
     // Verifica se o valor é uma string e remove os pontos que representam os milhares
     if (typeof value === 'string') {
       value = value.replace(/\./g, '');
