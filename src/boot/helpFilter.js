5478ed62748aa3e75c785525386342065d19d6dc
JorgeNunes <jlbnunes@live.com>
2024-12-21 17:52:59 -0300

    feat:update exchange

--- src/boot/helpFilter.js
+++ src/boot/helpFilter.js
@@ -36,11 +36,11 @@ const filters = {
     }).format(value);
   },
   /**
-   * converte para dolar data 09/12 $exchange
+   * converte para dolar data 21/12 $exchange
    * @param {String|Number} value valor que sera convertido para o cambio
    * @param {Number|null} exchange valor do câmbio para ser dividido no calculo
    */
-  convertCoin(value, exchange = 6.142301373) {
+  convertCoin(value, exchange = 6.15) {
     // Verifica se o valor é uma string e remove os pontos que representam os milhares
     if (typeof value === 'string') {
       value = value.replace(/\./g, '');
