3ff17c369ba2071eec788f7a0168a1d2f8b02315
Jorge Nunes <jlbnunes@live.com>
2025-03-12 12:49:59 -0300

    [REF]version adding

--- src/system/components/RegisterData.vue
+++ src/system/components/RegisterData.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="RegisterData">
-    <p>© {{ registerData }} Strategy Analytics</p>
+    <p>© {{ registerData }} Strategy Analytics v{{ version }}</p>
   </div>
 </template>
 <script>
 import { defineComponent } from "vue";
 export default defineComponent({
+  props:{
+    version:{type:[String, Boolean]}
+  },
   setup() {
     return {
       registerData: new Date().getFullYear(),
