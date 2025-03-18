f2127e40868729174a27a0d99bbb0af3328de5b5
JorgeNunes <jlbnunes@live.com>
2025-03-18 16:56:39 -0300

    [REF] updated exchange dollar

--- src/boot/helpFilter.js
+++ src/boot/helpFilter.js
@@ -36,11 +36,11 @@ const filters = {
     }).format(value);
   },
   /**
-   * converte para dolar data 02/02/2025 $exchange
+   * converte para dolar data 18/03/2025 $exchange
    * @param {String|Number} value valor que sera convertido para o cambio
    * @param {Number|null} exchange valor do câmbio para ser dividido no calculo
    */
-  convertCoin(value, exchange = 5.84) {
+  convertCoin(value, exchange = 5.66) {
     // Verifica se o valor é uma string e remove os pontos que representam os milhares
     if (typeof value === 'string') {
       value = value.replace(/\./g, '');
